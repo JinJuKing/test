759b09dd2e6dc0e1d32306bde4a9094aa184eeab
JinJuKing <bammay23@gmail.com>
2025-09-08 20:29:02 +0900

    코테 변경사항 완료

---
+++ 새 폴더/test22 - 복사본 (3).java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class test22 {
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		
+		String[] numList = input.split(",");
+		
+		for(String p : numList) {
+			int big = Integer.parseInt(p);
+			System.out.println(big);
+		}
+		
+		
+	}
+}
